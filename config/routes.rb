d19bcef24f90eee2f86721bcc7b390cc5c4f908e
RaghadAjamia <raghad.ajamia@gmaol.com>
2024-07-09 21:33:28 +0300

    some edits

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   get 'locations/userloc'
   get 'stations/userstations'
   get 'powerbanks/userpowerbanks'
+  get 'devise/admincreatsuser'
 
 
   resources :warehouses
